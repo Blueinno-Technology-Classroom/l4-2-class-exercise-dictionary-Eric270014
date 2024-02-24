9efa1aa4c0f9c56e46654f49b18f1da6e2256444
Eric <ericzjchee@gmail.com>
2024-02-24 15:06:41 +0800

    hi

--- main.py
+++ main.py
@@ -1,21 +1,58 @@
 ##################################################
 '''
 Q1: 
+
+
+print(sample_dict['history of magic'])
 '''
+
+
 
 # TODO: Write your code here
 
 ##################################################
 '''
 Q2:
+
 '''
+
+sample_dict = {
+        'emp1': {'name': 'Jhon', 'salary': 7500},
+        'emp2': {'name': 'Emma', 'salary': 8000},
+        'emp3': {'name': 'Brad', 'salary': 500}
+    }
+
+sample_dict['Brad']['Salary'] = 8500
+
 
 # TODO: Write your code here
 
 ##################################################
 '''
 Q3:
+
 '''
+
+mystery_pokemon = {
+     "height": 15,
+     "weight": 405,
+     "moves": [
+         {
+             "name": "lick",
+             "level_learned_at": 0
+         },
+         {
+             "name": "nightmare",
+             "level_learned_at": 0
+         },
+         {
+             "name": "curse",
+             "level_learned_at": 16
+         }
+     ]
+ }
+
+print(mystery_pokemon['moves'][2]['level_learned_at'])
 
 # TODO: Write your code here
 
